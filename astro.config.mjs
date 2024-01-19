d27948d1097e8957d5d36e4592079f93153d54cc
Adrián López <58343116+Kratogans@users.noreply.github.com>
2024-01-19 14:11:37 +0100

    Fix Deploy Github Pages
    Fix Deploy Github Pages | site: 'https://astronaut.github.io',
      base: '/my-repo',

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  /*Líneas para setear el deploy para github pages*/
+  site: 'https://Kratogans.github.io',
+  base: '/Portfolio',
+  /* Integración de tailwind */
   integrations: [tailwind()]
 });
